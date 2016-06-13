db23af1fcf9cdcb2fae7cbb420662bcc6a93bb33
Michael-Nunley <Michael.ray.nunley@gmail.com>
2016-06-13 02:40:35 -0700

    Removed demo code
    Moved stats to own window

--- main.cpp
+++ main.cpp
@@ -93,7 +93,7 @@ void updatehealth()
 }
 
 S_Stat S_Self_Stat[28];
-Nation N_Self = Nation("Default", E_History::Military_Coo);
+Nation N_Self /* = Nation("Default", E_History::Military_Coo)*/;
 void refreshStats()
 {
 	for (int i = 0; i < 28; i++)
@@ -174,17 +174,8 @@ int main(int, char**)
         // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets appears in a window automatically called "Debug"
         {
             ImGui::Text("Hello, world!");
-            if (ImGui::Button("Another Window")) show_another_window ^= 1;
-			static int slider[28] = { 20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20 };
-			if (ImGui::Button("Refresh Stats")) refreshStats();
-			for (int i = 0; i < 28; i++)
-			{
-				char buffertitle[256];
-				strncpy(buffertitle, E_StatNamesToChar((E_StatNames)i), sizeof(buffertitle));
-				strncpy(buffertitle, " Offset", sizeof(buffertitle));
-				ImGui::SliderInt(buffertitle, &slider[i], 1, 730);
-				ImGui::PlotLines(E_StatNamesToChar((E_StatNames)i), S_Self_Stat[i].AmountHistory.data(), 240, slider[i], "", 0.0f, 1000.0f, ImVec2(0, 80));
-			}
+            if (ImGui::Button("Show Nation Stats")) show_another_window ^= 1;
+			
 
 			if (ImGui::Button("Refresh Health")) updatehealth();
 			static int healthslidercount = 20;
@@ -202,59 +193,15 @@ int main(int, char**)
 			{
 				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 				ImGui::Text(N_Self.GetNationName());
-
-				static float arr[] = { 0.6f, 0.1f, 1.0f, 0.5f, 0.92f, 0.1f, 0.2f };
-				ImGui::PlotLines("Frame Times", arr, IM_ARRAYSIZE(arr));
-
-				static float values[90] = { 0 };
-				static int values_offset = 0;
-
-				static float refresh_time = ImGui::GetTime(); // Create dummy data at fixed 60 hz rate for the demo
-				for (; ImGui::GetTime() > refresh_time + 1.0f / 60.0f; refresh_time += 1.0f / 60.0f)
+				static int slider[28] = { 20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20 };
+				if (ImGui::Button("Refresh Stats")) refreshStats();
+				for (int i = 0; i < 28; i++)
 				{
-					static float phase = 0.0f;
-					values[values_offset] = cosf(phase);
-					values_offset = (values_offset + 1) % IM_ARRAYSIZE(values);
-					phase += 0.10f*values_offset;
+					ImGui::PlotLines(E_StatNamesToChar((E_StatNames)i), S_Self_Stat[i].AmountHistory.data(), 240, slider[i], "", 0.0f, 1000.0f, ImVec2(0, 80));
+					ImGui::SliderInt("", &slider[i], 1, 730, "");
+					ImGui::Separator();
 				}
-				ImGui::PlotLines("Lines", values, IM_ARRAYSIZE(values), values_offset, "avg 0.0", -1.0f, 1.0f, ImVec2(0, 80));
-				ImGui::PlotHistogram("Histogram", arr, IM_ARRAYSIZE(arr), 0, NULL, 0.0f, 1.0f, ImVec2(0, 80));
-
-				// Use functions to generate output
-				// FIXME: This is rather awkward because current plot API only pass in indices. We probably want an API passing floats and user provide sample rate/count.
-				struct Funcs
-				{
-					static float Sin(void*, int i) { return sinf(i * 0.1f); }
-					static float Saw(void*, int i) { return (i & 1) ? 1.0f : 0.0f; }
-				};
-				static int func_type = 0, display_count = 70;
-				ImGui::Separator();
-				ImGui::PushItemWidth(100); ImGui::Combo("func", &func_type, "Sin\0Saw\0"); ImGui::PopItemWidth();
-				ImGui::SameLine();
-				ImGui::SliderInt("Sample count", &display_count, 1, 500);
-				float(*func)(void*, int) = (func_type == 0) ? Funcs::Sin : Funcs::Saw;
-				ImGui::PlotLines("Lines", func, NULL, display_count, 0, NULL, -1.0f, 1.0f, ImVec2(0, 80));
-				ImGui::PlotHistogram("Histogram", func, NULL, display_count, 0, NULL, -1.0f, 1.0f, ImVec2(0, 80));
-				ImGui::Separator();
-
-				// Animate a simple progress bar
-				static float progress = 0.0f, progress_dir = 1.0f;
-				progress += progress_dir * 0.4f * ImGui::GetIO().DeltaTime;
-				if (progress >= +1.1f) { progress = +1.1f; progress_dir *= -1.0f; }
-				if (progress <= -0.1f) { progress = -0.1f; progress_dir *= -1.0f; }
-
-
-				// Typically we would use ImVec2(-1.0f,0.0f) to use all available width, or ImVec2(width,0.0f) for a specified width. ImVec2(0.0f,0.0f) uses ItemWidth.
-				ImGui::ProgressBar(progress, ImVec2(0.0f, 0.0f));
-				ImGui::SameLine(0.0f, ImGui::GetStyle().ItemInnerSpacing.x);
-				ImGui::Text("Progress Bar");
-
-				float progress_saturated = (progress < 0.0f) ? 0.0f : (progress > 1.0f) ? 1.0f : progress;
-				char buf[32];
-				sprintf(buf, "%d/%d", (int)(progress_saturated * 1753), 1753);
-				ImGui::ProgressBar(progress, ImVec2(0.f, 0.f), buf);
 			}
-
             ImGui::End();
         }
 
